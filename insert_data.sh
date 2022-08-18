477bc1adf5439f9212be5832a88d59eb9da29e1c
Snorkungen <teuvo2004@gmail.com>
2022-08-18 14:04:54 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,37 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+GET_TEAM_ID () {
+  TEAM_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$*';")"
+  echo $TEAM_ID
+}
+
+ADD_TEAM () {
+  TEAM_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$*';")"
+  if [[ -z $TEAM_ID ]]; then
+    TEAM_ID="$($PSQL "INSERT INTO teams (name) VALUES('$*');")"
+    echo "Added to teams: $*"
+  fi
+}
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS 
+do
+  if [[ $YEAR == year ]]; then continue; fi
+
+  ADD_TEAM $WINNER
+  ADD_TEAM $OPPONENT
+
+done
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS 
+do
+  if [[ $YEAR == year ]]; then continue; fi
+
+  WINNER_ID=$(GET_TEAM_ID $WINNER)
+  OPPONENT_ID=$(GET_TEAM_ID $OPPONENT)
+
+  $PSQL "INSERT INTO games (year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES ($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS);"
+
+done
+
